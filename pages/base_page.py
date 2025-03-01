72e9e534de95b4ad9c558eb062e836ff9801bdc2
Евгений <enbob.tester@gmail.com>
2025-03-01 16:52:46 +0300

    base_page.py

---
+++ pages/base_page.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def find_element(self, locator):
+        return self.wait.until(EC.visibility_of_element_located(locator))
+
+    def click_element(self, locator):
+        element = self.find_element(locator)
+        element.click()
+
+    def get_text(self, locator):
+        element = self.find_element(locator)
+        return str(element.text)
