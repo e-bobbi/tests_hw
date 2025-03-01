5e2dad9735db55e7156d16095e4e1f91003cff1c
Евгений <enbob.tester@gmail.com>
2025-03-01 16:55:25 +0300

    conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+@pytest.fixture
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--start-maximized")
+    service = Service('path/to/chromedriver')
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    yield driver
+    driver.quit()
